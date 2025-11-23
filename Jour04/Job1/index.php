73b32dc674484c34714099b95275db49116073fc
AnzizD <anzizdahalani@gmail.com>
2025-11-23 23:21:54 +0100

    Jour 4 - Job 1

---
+++ Jour04/Job1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Expression favorite</title>
+  </head>
+  <body>
+    <button id="button">Afficher mon expression</button>
+    <script src="script.js"></script>
+  </body>
+</html>
